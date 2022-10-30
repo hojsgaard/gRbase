871c9e8b4de464d1c192de5333474e5fd9dd876f
Søren Højsgaard <sorenh@math.aau.dk>
2022-10-30 18:51:52 +0100

    SH: SH

--- R/api_array_tab-create.R
+++ R/api_array_tab-create.R
@@ -6,23 +6,25 @@
 #' @author Søren Højsgaard, \email{sorenh@@math.aau.dk}
 ##################################################################
 #'
-#' @details If \code{normalize="first"} then for each configuration of
-#'     all other variables than the first, the probabilities are
-#'     normalized to sum to one.  Thus f(a,b,c) becomes a conditional
-#'     probability table of the form p(a|b,c).  If
-#'     \code{normalize="all"} then the sum over all entries of
+#' @details
+#'
+#' 1. If \code{normalize="first"} then for each configuration of all
+#'     other variables than the first, the probabilities are
+#'     normalized to sum to one.  Thus f(a, b, c) becomes a
+#'     conditional probability table of the form p(a | b, c).
+#'
+#' 1. If \code{normalize="all"} then the sum over all entries of
 #'     f(a,b,c) is one.
 #' 
-#'     If \code{smooth} is positive then
-#'     \code{smooth} is added to \code{values} before normalization
-#'     takes place.
+#' 1.  If \code{smooth} is positive then \code{smooth} is added to
+#'     \code{values} BEFORE normalization takes place.
 #' 
-#' @param names Names of variables defining table; a character vector
-#'     or a right hand sided formula.
+#' @param names Names of variables defining table; either a character
+#'     vector or a right hand sided formula.
 #' @param levels 1) a list with specification of the levels of the
 #'     factors in \code{names} or 2) a vector with number of levels of
 #'     the factors in \code{names}. See 'examples' below.
-#' @param values values to go into the parray
+#' @param values values to go into the array.
 #' @param normalize Either "none", "first" or "all". Should result be
 #'     normalized, see 'Details' below.
 #' @param smooth Should values be smoothed, see 'Details' below.
@@ -30,14 +32,14 @@
 #' @keywords utilities
 #' @examples
 #' 
-#' universe <- list(gender=c('male','female'),
-#'                  answer=c('yes','no'),
-#'                  rain=c('yes','no'))
-#' t1 <- tabNew(c("gender","answer"), levels=universe, values=1:4)
+#' universe <- list(gender=c('male', 'female'),
+#'                  answer=c('yes', 'no'),
+#'                  rain=c('yes', 'no'))
+#' t1 <- tabNew(c("gender", "answer"), levels=universe, values=1:4)
 #' t1
 #' t2 <- tabNew(~gender:answer, levels=universe, values=1:4)
 #' t2
-#' t3 <- tabNew(~gender:answer, c(2,2), values=1:4)
+#' t3 <- tabNew(~gender:answer, c(2, 2), values=1:4)
 #' t3
 NULL
 
@@ -79,9 +81,6 @@ tabNew <- function(names, levels, values, normalize="none", smooth=0){
     }
     tabNormalize(out, normalize)
 }
-
-
-
 
 ## FIXME .make.dimnames findes også (cirka) i parray. Redundans
 .make.dimnames <- function(names, levels){
