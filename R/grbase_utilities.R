37a194ac83ba75c7ef6db0135de660239967f4ff
Søren Højsgaard <sorenh@math.aau.dk>
2024-10-14 19:51:23 +0200

    SH: cleaning

--- R/grbase_utilities.R
+++ R/grbase_utilities.R
@@ -1,3 +1,20 @@
+
+.check.is.matrix <- function(x){
+    if (!inherits(x, c("matrix", "dgCMatrix")))
+        stop("Input must be a matrix or a dgCMatrix\n")
+}
+
+
+.check.is.igraph <- function(x){
+    if (!inherits(x, "igraph"))
+        stop("'x' not an igraph object...")    
+}
+
+.is_list_of_atomic <- function(z){
+    is.list(z) && all(sapply(z, is.atomic))            
+}
+
+
 ## ###################################################################
 ##
 #' @title gRbase utilities
